51847a3b4ed0950a065d01c926909a0eb3ac4539
Nycolas da Guia Santos <50758229+Nycolas-Guia@users.noreply.github.com>
2023-12-01 10:54:11 -0300

    Add files via upload

--- calculaForma.php
+++ calculaForma.php
@@ -9,82 +9,82 @@ class calculaForma
     //calculo do volume do cilindro
     function volumeCilindro($raio,$altura)
     {        
-        return pi()*$raio*(2*$altura); //πa2b        
+        return pi() * pow($raio,2) * $altura; //πa²b        
     }
 	
     //calculo da área da base do cilindro
     function areaBaseCilindro($raio){
-
+        return pi()* pow($raio, 2); //πa²
     }
 
     //calculo da área lateral do cilindro
     function areaLateralCilindro($raio, $altura){
-
+        return 2 * pi() * $raio * $altura; //2πab
     }
 
     //calculo da área total do cilindro
-    function areaTotalCilindro(){
-
+    function areaTotalCilindro($raio, $altura){
+        return 2 * pi() * pow($raio,2) * 2 * pi() * $raio * $altura; //2πa²2πab
     }
 
     //calculo do perímetro do cilindro
-    function perimetroCilindro(){
-        
+    function perimetroCilindro($raio){
+        return 2 * pi() * $raio; //2πa
     }
 
     //calculo da área do circulo
-    function areaCirculo(){
-
+    function areaCirculo($raio){
+        return pi() * pow($raio,2); //πa²
     }
 
     //calculo do perímetro do circulo
-    function perimetroCirculo(){
-
+    function perimetroCirculo($raio){
+        return 2 * pi() * $raio; //2πa
     }
 
     //calculo da área do quadrado
-    function areaQuadrado(){
-
+    function areaQuadrado($base, $altura){
+        return $base * $altura; //ab
     }
 
     //calculo do volume do quadrado
-    function volumeQuadrado(){
-
+    function volumeQuadrado($base, $altura, $profundidade){
+        return  $base * $altura * $profundidade; //abc
     }
 
     //calculo do perímetro do quadrado
-    function perimetroQuadrado(){
-
+    function perimetroQuadrado($base, $altura){
+        return (2 * $base) + (2 * $altura); //2a+2b
     }
 
     //calculo da área do triangulo equilatero
-    function areaEquilatero(){
-
+    function areaEquilatero($altura){
+        return pow($altura,2) / sqrt(3); //a²/√3
     }
 
     //calculo do volume do triangulo equilatero
-    function volumeEquilatero(){
-
+    function volumeEquilatero($altura, $profundidade){
+        return (pow($altura,2)/3) * $profundidade; //(a²/3)b
     }
 
     //calculo do perímetro do triangulo equilatero
-    function perimetroEquilatero(){
-
+    function perimetroEquilatero($altura){
+        return 3 * $altura;//3a
     }
 
     //calculo da área do triangulo isosceles
-    function areaIsosceles(){
-
+    function areaIsosceles($base, $altura){
+        return ($base * $altura) / 2;//ab/2
     }
 
     //calculo do volume do triangulo isosceles
-    function volumeIsosceles(){
-
+    function volumeIsosceles($base, $altura, $profundidade){
+        return (($base * $altura) / 2) * $profundidade;//(ab/2)c
     }
 
     //calculo do perímetro do triangulo isosceles
     function perimetroIsosceles(){
-
+        return ;//2(√(a²+(b/2)²+b)
     }
 
     //calculo da área do triangulo retangulo
